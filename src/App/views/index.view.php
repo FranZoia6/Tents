44ae2435d24956fd143f2c6910084ee9112af803
Enzo Federico Bianchi <enzobianchi_8@hotmail.com>
2024-03-11 18:37:58 -0300

    actualizo

--- src/App/views/index.view.php
+++ src/App/views/index.view.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/public/style/reset.css" rel="stylesheet" type="text/css">
-    <link href="/public/style/styles.css" rel="stylesheet" type="text/css">
+    <link href="./../style/reset.css" rel="stylesheet" type="text/css">
+    <link href="./../style/styles.css" rel="stylesheet" type="text/css">
 
     <title>Document</title>
 </head>
@@ -23,13 +23,13 @@
             <p>
                 Sumérgete en una experiencia de playa inolvidable con nuestro exclusivo complejo de alquiler de sombrillas y carpas en la costa. Disfruta del sol y el mar con comodidad absoluta en nuestras modernas carpas equipadas, mientras nuestro personal atento se encarga de todos los detalles. Además, relájate en nuestra refrescante pileta con vista al océano o disfruta de una amplia variedad de bebidas y platos frescos en nuestra cantina playera. Te invitamos a vivir la combinación perfecta de relajación, diversión y servicios de primera en tu día de playa con nosotros.
             </p>
-            <img src="/public/imagenes/carpas2.jpg" alt="Carpas1">
+            <img src="./../imagenes/carpas2.jpg" alt="Carpas1">
         </section>
     
         <section>
             <h2>Carpas</h2>
             <figure>
-                <img src="/public/imagenes/carpas2.jpg" alt="carpa">
+                <img src="./../imagenes/carpas2.jpg" alt="carpa">
             </figure>
             <p class="parrafo">
                 Un servicio de carpa en la playa de Mar del Plata brinda la oportunidad de disfrutar del sol y el mar con comodidad y estilo. Con carpas privadas equipadas con mobiliario, atención personalizada para bebidas y alimentos, junto con toallas y servicio de limpieza, ofrece una experiencia relajada y sin preocupaciones. Además, la posibilidad de reservar anticipadamente asegura un espacio ideal para aprovechar al máximo el día en la playa.
@@ -38,7 +38,7 @@
         <section>
             <h2>Sombrillas</h2>
             <figure>
-                <img src="/public/imagenes/mesa1.png" alt="carpa">
+                <img src="./../imagenes/mesa1.png" alt="carpa">
             </figure>
             <p class="parrafo">
                 Nuestro servicio de mesa con sombrilla redefine la experiencia playera, ofreciéndote un oasis personal en la costa. Con nuestras mesas estratégicamente ubicadas y equipadas con cómodas sillas, te brindamos la oportunidad de disfrutar del sol y el mar con la máxima comodidad. La sombrilla adjunta proporciona sombra para momentos de relajación mientras te sumerges en la brisa marina. Nuestro objetivo es ofrecerte un espacio exclusivo y acogedor para que disfrutes de cada momento en la playa con tranquilidad y confort
--- src/App/views/internal-error.view.php
+++ src/App/views/internal-error.view.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/public/style/reset.css" rel="stylesheet" type="text/css">
-    <link href="/public/style/styles.css" rel="stylesheet" type="text/css">
+    <link href="./../style/reset.css" rel="stylesheet" type="text/css">
+    <link href="./../style/styles.css" rel="stylesheet" type="text/css">
 
     <title>Not found</title>
 </head>
