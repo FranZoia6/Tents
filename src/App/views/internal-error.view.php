e7a054f738d8412ec78e0e804a20092de976b69a
FranZoia6 <francozoiamoque@hotmail.com>
2024-03-27 20:37:56 +0100

    Incorporacion de Recursos Estaticos

--- src/App/views/internal-error.view.php
+++ src/App/views/internal-error.view.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./../style/reset.css" rel="stylesheet" type="text/css">
-    <link href="./../style/styles.css" rel="stylesheet" type="text/css">
-
-    <title>Not found</title>
-</head>
+<?php
+    require 'parts/head.view.php'
+?>
 <body>
-
-    
+<body>
     
     <?php
         require 'parts/footer.view.php'
--- src/App/views/services.view.php
+++ src/App/views/services.view.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./../style/reset.css" rel="stylesheet" type="text/css">
-    <link href="./../style/styles.css" rel="stylesheet" type="text/css">
-
-    <title>Contacto</title>
-</head>
+<?php
+    require 'parts/head.view.php'
+?>
+<body>
 <body>
     <header>
     </header>
